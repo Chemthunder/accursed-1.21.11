4ffd0316708815feee5f9673fff126d02c7a5ab2
Chemthunder <chemthunderisme@gmail.com>
2026-04-05 20:54:03 -0400

    im a dead girl walking

--- src/main/java/net/kindling/accursed/impl/index/AccursedDataComponents.java
+++ src/main/java/net/kindling/accursed/impl/index/AccursedDataComponents.java
@@ -1,12 +1,12 @@
 package net.kindling.accursed.impl.index;
 
-import net.acoyt.acornlib.api.registrants.ComponentTypeRegistrant;
+import net.acoyt.acornlib.api.registrants.DataComponentTypeRegistrant;
 import net.kindling.accursed.impl.Accursed;
 import net.kindling.accursed.impl.util.Etching;
 import net.minecraft.component.ComponentType;
 
 public interface AccursedDataComponents {
-    ComponentTypeRegistrant DATA_COMPONENTS = new ComponentTypeRegistrant(Accursed.MOD_ID);
+    DataComponentTypeRegistrant DATA_COMPONENTS = new DataComponentTypeRegistrant(Accursed.MOD_ID);
 
     ComponentType<Etching> STORED_ETCHING = DATA_COMPONENTS.register("stored_etching", etchingBuilder -> etchingBuilder.codec(Etching.CODEC));
 
--- src/main/java/net/kindling/accursed/impl/util/Etching.java
+++ src/main/java/net/kindling/accursed/impl/util/Etching.java
@@ -2,6 +2,7 @@ package net.kindling.accursed.impl.util;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 
@@ -9,7 +10,7 @@ public record Etching(String key, Ingredient itemNeededToCraft, int color) {
     public static final Codec<Etching> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
                     Codec.STRING.optionalFieldOf("key", "etching.accursed.empty").forGetter(Etching::key),
-                    Ingredient.CODEC.optionalFieldOf("itemNeededToCraft", Ingredient.ofItem(Items.AIR)).forGetter(Etching::itemNeededToCraft),
+                    Ingredient.CODEC.optionalFieldOf("itemNeededToCraft", Ingredient.ofItem(Items.SUNFLOWER)).forGetter(Etching::itemNeededToCraft),
                     Codec.INT.optionalFieldOf("color", 0xFFffffff).forGetter(Etching::color)
             ).apply(instance, Etching::new)
     );
